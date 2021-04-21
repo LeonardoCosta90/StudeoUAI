9dac4dd26a65620d97d46ef8594e08a35928149a
LeonardoCosta90 <leonardoeverton.tec@gmail.com>
2021-04-20 21:49:51 -0300

    feat: home and widgets

--- mobile/studeo_uai/lib/main.dart
+++ mobile/studeo_uai/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:studeo_uai/splash/splash_page.dart';
+import 'package:studeo_uai/core/app_widget.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'StudeoUAI',
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      home: AppWidget(),
     );
   }
 }
