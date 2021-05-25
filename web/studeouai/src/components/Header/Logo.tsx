509cb8cf8fee40494b40b51c2e1e49b0b2cd96e9
unknown <leonardo.costa@EXT-ENG26.exsto.local>
2021-05-25 09:51:52 -0300

    refactor: header responsive

--- web/studeouai/src/components/Header/Logo.tsx
+++ web/studeouai/src/components/Header/Logo.tsx
@@ -3,14 +3,14 @@ import { Text } from "@chakra-ui/react";
 export function Logo() {
   return (
     <Text
-      fontSize="3xl"
+      fontSize={['2xl', '3xl']}
       fontWeight="bold"
       letterSpacing="tight"
       w="64"
       color="purple.500"
-      >
-        STUDEO
-        <Text as="span"color="green.500" >UAI</Text>
-      </Text>
+    >
+      STUDEO
+      <Text as="span" color="green.500" >UAI</Text>
+    </Text>
   );
 }
--- web/studeouai/src/components/Header/Profile.tsx
+++ web/studeouai/src/components/Header/Profile.tsx
@@ -1,14 +1,19 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react"
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean
+}
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
-      <Flex align="center">
-         <Box mr="4" textAlign="right">
-           <Text>Leonardo Costa</Text>
-           <Text color="gray.300" fontSize="small">leonardoeverton.tec@gmail.com</Text>          
-         </Box>
-         <Avatar size="md" name="Leonardo Costa" src="https://github.com/LeonardoCosta90.png"/>
-       </Flex>
-     
+    <Flex align="center">
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Leonardo Costa</Text>
+          <Text color="gray.300" fontSize="small">leonardoeverton.tec@gmail.com</Text>
+        </Box>
+      )}
+      <Avatar size="md" name="Leonardo Costa" src="https://github.com/LeonardoCosta90.png" />
+    </Flex>
+
   );
 }
