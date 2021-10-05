96b39bd6d35f37911c3e493c36bfdc6ab3bd348a
Leonardo Costa <leonardo.costa@toodoo.com.br>
2021-10-05 17:11:08 -0300

    feat: specfication class

---
+++ backend/studeouai-api/src/modules/class/controllers/specifications-class-controller.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import { SpecificationClassService } from '../services/class-specification-service';
+
+export class SpecificationClassController {
+  async specificationClass(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ): Promise<Response> {
+    try {
+      const { id } = request.params;
+      const { specifications_ids } = request.body;
+
+      const specificationClassService = new SpecificationClassService();
+      const _class = await specificationClassService.createSpecificationClass({
+        class_id: id,
+        specifications_ids,
+      });
+
+      return response.status(201).json(_class);
+    } catch (err) {
+      next(err);
+    }
+  }
+}
